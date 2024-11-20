f375a866d0325fb2b2ad9e1eec2514f51e655578
NevinRashid <nevinalirashid@gmail.com>
2024-11-20 20:05:41 +0300

    add the dashboard and create the database tables

--- app/Models/User.php
+++ app/Models/User.php
@@ -41,4 +41,16 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    public function rating(){
+        return $this->hasOne(Rating::class);
+    }
+
+    public function comments(){
+        return $this->hasMany(Comment::class);
+    }
+
+    public function courses() {
+        return $this->belongsToMany(Course::class,'course_user','user_id','course_id');
+    }
 }
